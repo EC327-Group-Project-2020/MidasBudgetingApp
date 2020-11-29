f078a1d5030d8205922f3a416876aeb60754bbad
nannkat <nannkat@bu.edu>
2020-11-28 20:37:44 -0500

    Empty display page and corresponding layout created

---
+++ app/src/main/java/com/example/personalfinanceplanner/BudgetDisplayPage.java
@@ -0,0 +1,14 @@
+package com.example.personalfinanceplanner;
+
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class BudgetDisplayPage extends AppCompatActivity implements View.OnClickListener {
+
+
+    @Override
+    public void onClick(View v) {
+
+    }
+}
