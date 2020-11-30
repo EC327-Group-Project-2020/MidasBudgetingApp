5d7c6337ab9005df36efde0f9ee1522e0fae9f84
kevinvogtlowell <kjv@bu.edu>
2020-11-30 10:05:44 -0500

    Added User copy constructor, linked Log In page to Budget Display page. Logging in now passes user object with login info to the main page, where the object is received and able to be used and updated in database.
    Also added list of saved currencies as a data field for users, with type converters to convert the list data into strings that can be stored and room and back to arrays when pulled from room.

--- app/src/main/java/com/example/personalfinanceplanner/User.java
+++ app/src/main/java/com/example/personalfinanceplanner/User.java
@@ -3,11 +3,14 @@ package com.example.personalfinanceplanner;
 import androidx.annotation.NonNull;
 import androidx.room.ColumnInfo;
 import androidx.room.Entity;
-import androidx.room.Fts4;
 import androidx.room.PrimaryKey;
+import androidx.room.TypeConverters;
+
+import java.io.Serializable;
+import java.util.ArrayList;
 
 @Entity(tableName = "user_info")
-public class User {
+public class User implements Serializable {
 
     @PrimaryKey(autoGenerate = true)
     public long userID;
@@ -36,11 +39,13 @@ public class User {
     @ColumnInfo(name = "security_answer_two")
     private String securityAnswerTwo;
 
-    @ColumnInfo(name = "alternate_currency")
-    private String alternateCurrency;
+    @NonNull
+    @TypeConverters(Converters.class)
+    @ColumnInfo(name = "saved_currency_list")
+    private ArrayList<String> savedCurrencies = new ArrayList<String>(); //creates empty resizeable array
 
     //constructor
-    public User(String username, String password, String securityQuestionOne, String securityQuestionTwo, String securityAnswerOne, String securityAnswerTwo, String alternateCurrency)
+    public User(String username, String password, String securityQuestionOne, String securityQuestionTwo, String securityAnswerOne, String securityAnswerTwo)
     {
         this.username = username;
         this.password = password;
@@ -48,7 +53,19 @@ public class User {
         this.securityQuestionTwo = securityQuestionTwo;
         this.securityAnswerOne = securityAnswerOne;
         this.securityAnswerTwo = securityAnswerTwo;
-        this.alternateCurrency = alternateCurrency;
+        this.savedCurrencies.add("USD");
+    }
+
+    //copy constructor
+    public User(User user)
+    {
+        this.username = user.getUsername();
+        this.password = user.getPassword();
+        this.securityQuestionOne = user.getSecurityQuestionOne();
+        this.securityQuestionTwo = user.getSecurityQuestionTwo();
+        this.securityAnswerOne = user.getSecurityAnswerOne();
+        this.securityAnswerTwo = user.getSecurityAnswerTwo();
+        this.savedCurrencies = user.getSavedCurrencies();
     }
 
     /*NOTE: getter and setter names below should NOT be altered. In order to keep
@@ -57,6 +74,7 @@ public class User {
     is reflected in the names chosen for those member functions
      */
 
+    //accessors and mutators
     public void setUsername(String new_name) {
         username = new_name;
     }
@@ -105,7 +123,34 @@ public class User {
         return securityAnswerTwo;
     }
 
-    public void setAlternateCurrency(String newAlternateCurrency) { alternateCurrency = newAlternateCurrency; }
+    public void setSavedCurrencies(ArrayList<String> newSavedCurrency) { savedCurrencies = newSavedCurrency; }
 
-    public String getAlternateCurrency() { return alternateCurrency; }
+    public ArrayList<String> getSavedCurrencies() { return savedCurrencies; }
+
+    //other member functions
+
+    public void addSavedCurrency(String newCurrency) { //adds a new currency to the favorite list, and checks for duplicates
+
+        //check to make sure that currency hasn't already been added to list
+        for (int i = 0; i < savedCurrencies.size(); i++)
+        {
+            if (savedCurrencies.get(i).equals(newCurrency))
+            {
+                System.out.println("Error: currency type has already been added to saved list.");
+                return;
+            }
+        }
+
+        savedCurrencies.add(newCurrency);
+    }
+
+    public void removeSavedCurrency(String currencyToRemove) {  //removes specified saved currency, but does not allow "USD" to be removed
+        if (currencyToRemove.equals("USD"))
+        {
+            System.out.println("Error: cannot delete default currency.");
+        }
+        else
+            savedCurrencies.remove(currencyToRemove);
+    }
+
 }
